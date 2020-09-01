d7ef06490215754d22a3138fb412d4df61b392b5
Ehtisham Zaheer <ehtisham_zaheer@hotmail.com>
2020-09-01 16:34:36 +0200

    WebAPi-Ex-2 Result with specific Id producing erro

---
+++ Ex-2-WebApi/Ex-2-WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ex_2_WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ex_2_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+                Id = 1006368,
+                Name = "Austin and Barbque AABQ Wifi food thrmometer",
+                Description = "Thermometer med Wifi för an optimal inntertemperatur",
+                Price = 399
+            },
+            new Product
+            {
+                Id = 1009334,
+                Name = "Andersson Electrisk Tändare ECL 1.1",
+                Description = "Electrisk Stromsäker tändare helt utan gas och bränsle",
+                Price = 89
+            },
+            new Product
+            {
+                Id = 1002266,
+                Name = "Weber Non-sticky Spray",
+                Description = "BBQ-oljespray som motverker at råvaror fastnar på gallret",
+                Price = 99
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("id")]
+        public Product Get(int id)
+        {
+            var product = products.Find(product => product.Id == id);
+            return product;
+        }
+
+    }
+}
