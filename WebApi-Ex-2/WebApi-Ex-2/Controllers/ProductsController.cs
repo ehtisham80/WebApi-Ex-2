de05455340386783d16fe5095f2d3460c474cbdc
Ehtisham Zaheer <ehtisham_zaheer@hotmail.com>
2020-09-01 15:14:07 +0200

    webApi-ex-2

---
+++ WebApi-Ex-2/WebApi-Ex-2/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_Ex_2.Models;
+
+namespace WebApi_Ex_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product{
+            Id= 1006368,
+            Name= "Austin and Barbeque AABQ Wifi food Thermometer",
+            Description= "Thermometer med Wifi för an optimal innertempratur",
+            Price=399
+            },
+            new Product
+            {
+            Id= 1009334,
+            Name= "Anderson Electric Tändare ECL 1.1",
+            Description= "Electrisk Störmsäker tänder helt utan gas och bränsle",
+            Price=89
+            },
+            new Product{
+            Id= 100266,
+            Name= "Weber non-stick spray",
+            Description= "BBQ oljespray som motverker att råvaror fastnar på gallret",
+            Price=99
+            }
+        };
+
+
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    }
+}
