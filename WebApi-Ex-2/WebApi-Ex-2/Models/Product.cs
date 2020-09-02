2fb661bc7d75155883da9eaa8572d08df93a107f
Ehtisham Zaheer <ehtisham_zaheer@hotmail.com>
2020-09-02 14:52:34 +0200

    crud.AcctionResult

--- WebApi-Ex-2/WebApi-Ex-2/Models/Product.cs
+++ WebApi-Ex-2/WebApi-Ex-2/Models/Product.cs
@@ -11,7 +11,7 @@ namespace WebApi_Ex_2.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
-
+       
         public Product()
         {
 
